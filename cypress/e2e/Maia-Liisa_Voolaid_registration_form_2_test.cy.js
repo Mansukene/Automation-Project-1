726d2f00aea2cd089c7476b2a46e82c83003ccf7
Mansukene <maialiisa.voolaid@gmail.com>
2024-05-09 20:51:01 +0300

    Last version

--- cypress/e2e/Maia-Liisa_Voolaid_registration_form_2_test.cy.js
+++ cypress/e2e/Maia-Liisa_Voolaid_registration_form_2_test.cy.js
@@ -62,7 +62,7 @@ describe('Section 1: Functional tests', () => {
 
         // example, how to use function, which fills in all mandatory data
         // in order to see the content of the function, scroll to the end of the file
-        inputValidData('johnDoe')
+        //inputValidData('johnDoe')
     })
 
     it('User cannot submit the form when the email address is absent', ()=>{
@@ -95,8 +95,6 @@ describe('Section 1: Functional tests', () => {
         // This test has failed, because the error message is not visible (in the elements it is display: none) and when I try manually, it doesn't appear too, did I find a bug?
     })
 
-    // Add at least 1 test for checking some mandatory field's absence
-
 })
 
 /*
@@ -114,10 +112,16 @@ describe('Section 2: Visual tests', () => {
     })
 
     it('My test for second picture', () => {
-        // Create similar test for checking the second picture
+        cy.log('Will check second logo source and size')
+        cy.get('[data-cy="cypress_logo"]').should('have.attr', 'src').should('include', 'cypress_logo')
+        cy.get('[data-cy="cypress_logo"]').invoke('height').should('be.lessThan', 116)
+            .and('be.greaterThan', 80)
+        cy.get('[data-cy="cypress_logo"]').invoke('width').should('be.lessThan', 120)
+            .and('be.greaterThan', 115)  
     });
 
     it('Check navigation part', () => {
+        cy.log('Will check Registration form 1 navigation')
         cy.get('nav').children().should('have.length', 2)
 
         // Get navigation element, find siblings that contains h1 and check if it has Registration form in string
@@ -136,44 +140,56 @@ describe('Section 2: Visual tests', () => {
         cy.log('Back again in registration form 2')
     })
 
-    // Create similar test for checking the second link 
+    it('Check navigation part for second link', () => {
+        cy.log('Will check Registration form 2 navigation')
+        cy.get('nav').children().should('have.length', 2)
+        cy.get('nav').siblings('h1').should('have.text', 'Registration form number 2')
+        cy.get('nav').children().eq(1).should('be.visible')
+            .and('have.attr', 'href', 'registration_form_3.html')
+            .click()
+        cy.url().should('contain', '/registration_form_3.html')
+        cy.go('back')
+        cy.log('Back again in registration form 2')
+    })
 
     it('Check that radio button list is correct', () => {
-        // Array of found elements with given selector has 4 elements in total
+        cy.log('Will check that radio button list is correct')
         cy.get('input[type="radio"]').should('have.length', 4)
-
-        // Verify labels of the radio buttons
         cy.get('input[type="radio"]').next().eq(0).should('have.text','HTML')
         cy.get('input[type="radio"]').next().eq(1).should('have.text','CSS')
         cy.get('input[type="radio"]').next().eq(2).should('have.text','JavaScript')
         cy.get('input[type="radio"]').next().eq(3).should('have.text','PHP')
-
-        //Verify default state of radio buttons
         cy.get('input[type="radio"]').eq(0).should('not.be.checked')
         cy.get('input[type="radio"]').eq(1).should('not.be.checked')
         cy.get('input[type="radio"]').eq(2).should('not.be.checked')
         cy.get('input[type="radio"]').eq(3).should('not.be.checked')
-
-        // Selecting one will remove selection from the other radio button
         cy.get('input[type="radio"]').eq(0).check().should('be.checked')
         cy.get('input[type="radio"]').eq(1).check().should('be.checked')
         cy.get('input[type="radio"]').eq(0).should('not.be.checked')
     })
 
-    // Create test similar to previous one verifying check boxes
+    it('Check that checkbox list is correct', () => {
+        cy.log('Will check that checkbox list is correct')
+        cy.get('input[type="checkbox"]').should('have.length', 3)
+        cy.get('input[type="checkbox"]').next().eq(0).should('have.text','I have a bike')
+        cy.get('input[type="checkbox"]').next().eq(1).should('have.text','I have a car')
+        cy.get('input[type="checkbox"]').next().eq(2).should('have.text','I have a boat')
+        cy.get('input[type="checkbox"]').eq(0).should('not.be.checked')
+        cy.get('input[type="checkbox"]').eq(1).should('not.be.checked')
+        cy.get('input[type="checkbox"]').eq(2).should('not.be.checked')
+
+        // All checkboxes can be checked
+        cy.get('input[type="radio"]').eq(0).check().should('be.checked')
+        cy.get('input[type="radio"]').eq(1).check().should('be.checked')
+        cy.get('input[type="radio"]').eq(2).check().should('be.checked')
+    })
 
     it('Car dropdown is correct', () => {
-        // Here is just an example how to explicitely create screenshot from the code
-        // Select second element and create screenshot for this area or full page
+        cy.log('Will check that car dropdown is correct')
         cy.get('#cars').select(1).screenshot('Cars drop-down')
         cy.screenshot('Full page screenshot')
-
-        // Here are given different solutions how to get the length of array of elements in Cars dropdown
-        // Next 2 lines of code do exactly the same!
         cy.get('#cars').children().should('have.length', 4)
         cy.get('#cars').find('option').should('have.length', 4)
-        
-        // Check  that first element in the dropdown has text Volvo
         cy.get('#cars').find('option').eq(0).should('have.text', 'Volvo')
         
         // Advanced level how to check the content of the Cars dropdown
@@ -183,18 +199,30 @@ describe('Section 2: Visual tests', () => {
         })
     })
 
-    // Create test similar to previous one
-
+    it('Favorite animal dropdown is correct', () => {
+        cy.log('Will check that favorite animal dropdown is correct')
+        cy.get('#animal').select(1).screenshot('Favorite animal drop-down')
+        cy.screenshot('Full page screenshot')
+        cy.get('#animal').children().should('have.length', 6)
+        cy.get('#animal').find('option').should('have.length', 6)
+        cy.get('#animal').find('option').eq(1).should('have.text', 'Cat')
+        
+        // Advanced level how to check the content of the Cars dropdown
+        cy.get('#animal').find('option').then(options => {
+            const actual = [...options].map(option => option.value)
+            expect(actual).to.deep.eq(['dog', 'cat', 'snake', 'hippo', 'cow','mouse'])
+        })
+    })
+    
 })
 
-function inputValidData(username) {
-    cy.log('Username will be filled')
-    cy.get('input[data-testid="user"]').type(username)
-    cy.get('#email').type('validemail@yeap.com')
-    cy.get('[data-cy="name"]').type('John')
-    cy.get('#lastName').type('Doe')
-    cy.get('[data-testid="phoneNumberTestId"]').type('10203040')
-    cy.get('#password').type('MyPass')
-    cy.get('#confirm').type('MyPass')
-    cy.get('h2').contains('Password').click()
-}
+//function inputValidData(username) {
+    //cy.log('Username will be filled')
+    //cy.get('input[data-testid="user"]').type(username)
+    //cy.get('#email').type('validemail@yeap.com')
+    //cy.get('[data-cy="name"]').type('John')
+    //cy.get('#lastName').type('Doe')
+    //cy.get('[data-testid="phoneNumberTestId"]').type('10203040')
+    //cy.get('#password').type('MyPass')
+    //cy.get('#confirm').type('MyPass')
+    //cy.get('h2').contains('Password').click()
